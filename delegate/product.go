4b52f219758c9ad4a59b16b2d0ad6bd49613c4ea
KenWilliamson <kenulborapublic@gmail.com>
2026-02-23 16:19:53 -0500

    update

--- delegate/product.go
+++ delegate/product.go
@@ -6,25 +6,25 @@ func (s *Service) GetProduct(sku string) *Product {
 	i1.ID = 12345
 	i1.SKU = "123444"
 	i1.Description = "System76 Laptop"
-	i1.Price = 1924.00
+	i1.Price = 1924.66
 
 	var i2 Product
 	i2.ID = 258444
 	i2.SKU = "2558444"
 	i2.Description = "System76 Monitor"
-	i2.Price = 798.00
+	i2.Price = 798.56
 
 	var i3 Product
 	i3.ID = 66558
 	i3.SKU = "6655444777"
 	i3.Description = "System76 Mouse"
-	i3.Price = 78.00
+	i3.Price = 78.99
 
 	var i4 Product
 	i4.ID = 474748
 	i4.SKU = "4561122"
 	i4.Description = "System76 Hard Drive 1TB"
-	i4.Price = 285.00
+	i4.Price = 285.50
 
 	var ilist []Product
 	ilist = append(ilist, i1, i2, i3, i4)
